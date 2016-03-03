44b5a406c04a67eeacaf695e267cc73dd8f6570d
JuanitoFatas <katehuang0320@gmail.com>
2016-03-03 11:03:14 +0800

    Add twemoji support, update ruby asia paragraph

--- app/view.rb
+++ app/view.rb
@@ -1,5 +1,6 @@
 require "json"
 require "date"
+require "twemoji"
 
 class View
   attr_reader :repos
@@ -8,6 +9,10 @@ class View
     @repos = Dir["data/repos/**/repos-#{recent_period}.json"].
       flat_map { |region| JSON.parse(IO.read(region))["repos"] }.
       sort_by { |r| r["stars"] }.reverse
+  end
+
+  def emojify(content)
+    Twemoji.parse(content, file_ext: ".svg")
   end
 
   private
