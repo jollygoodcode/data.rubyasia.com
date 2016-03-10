1335ab00f9f668a76efae8c063e7e5dd4cd34cd2
JuanitoFatas <katehuang0320@gmail.com>
2016-03-10 11:01:21 +0800

    Do the work everyday at 01:00 (GMT +8) and health report every hour

--- config/clockwork.rb
+++ config/clockwork.rb
@@ -3,10 +3,14 @@ require_relative "../app/daily_job"
 require_relative "../app/slack"
 
 module Clockwork
-  every(1.day, "Daily Job", tz: "Singapore", at: "09:00") do
+  every(1.day, "Daily Job", tz: "Singapore", at: "01:00") do
     if DailyJob.perform_now
       Slack.ping!
     end
+  end
+
+  every(1.hour, "Health Check", at: "**:00") do
+    Slack.ping!("I am still alive!")
   end
 
   error_handler do |error|
