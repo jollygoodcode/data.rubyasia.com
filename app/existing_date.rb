e7f07abda360f5ccdd1861b0df4e982a49ca2b17
JuanitoFatas <katehuang0320@gmail.com>
2016-08-01 16:55:33 +0800

    Remove unnecessary variable assignment

--- app/existing_date.rb
+++ app/existing_date.rb
@@ -3,7 +3,7 @@ require "date"
 
 class ExistingDate
   def self.all
-    periods = Dir["data/repos/**/*.json"].map do |path|
+    Dir["data/repos/**/*.json"].map do |path|
       path.match(/2016-\d{2}-\d{2}_2016-\d{2}-\d{2}/).to_s
     end.uniq.sort_by do |period|
       get_end_date(period)
