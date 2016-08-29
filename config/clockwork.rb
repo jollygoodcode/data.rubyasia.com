0a1a1527d841e39d5e8e17374af86a3ab6749ece
JuanitoFatas <katehuang0320@gmail.com>
2016-08-29 22:49:01 +0800

    Don't ping on Slack anymore

--- config/clockwork.rb
+++ config/clockwork.rb
@@ -4,26 +4,10 @@ require_relative "../app/slack"
 
 module Clockwork
   every(1.day, "Daily Job", tz: "Singapore", at: "15:00") do
-    success = DailyJob.sync_repo_and_restart_clockwork
-
-    if success
-      Slack.ping!("Repository synced and restarted clockwork")
-    else
-      Slack.ping!("Repository synced failed.")
-    end
+    DailyJob.sync_repo_and_restart_clockwork
   end
 
   every(1.day, "Daily Job", tz: "Singapore", at: "16:00") do
-    sent = DailyJob.perform_now
-
-    if sent && sent.respond_to?(:html_url)
-      Slack.ping!("Pull Request opened: #{sent.html_url}")
-    else
-      Slack.ping!("Something went wrong!")
-    end
-  end
-
-  error_handler do |error|
-    Slack.log!("[data.rubyasia.com] " + error.to_s)
+    DailyJob.perform_now
   end
 end
